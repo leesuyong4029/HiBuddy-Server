774cdaf3caed9ce962ef5ef768034219517d8ac3
suyong <edgarlee9364@gmail.com>
2024-05-19 18:48:01 +0900

    chore: S3 의존성 추가 & refresh_token 필드 추가

--- src/main/java/com/example/HiBuddy/domain/oauth/google/GoogleTokenResponse.java
+++ src/main/java/com/example/HiBuddy/domain/oauth/google/GoogleTokenResponse.java
@@ -5,6 +5,7 @@ import lombok.Data;
 @Data
 public class GoogleTokenResponse {
     private String access_token;
+    private String refresh_token;
     private String token_type;
     private Long expires_in;
     private String id_token;
