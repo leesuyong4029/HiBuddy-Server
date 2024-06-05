e1694cac008918a09d144004b646125f19eb25d6
suyong <edgarlee9364@gmail.com>
2024-06-06 01:00:20 +0900

    Fix: @Table 추가

--- src/main/java/com/example/HiBuddy/domain/image/Images.java
+++ src/main/java/com/example/HiBuddy/domain/image/Images.java
@@ -12,6 +12,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@Table(name = "images")
 public class Images extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
