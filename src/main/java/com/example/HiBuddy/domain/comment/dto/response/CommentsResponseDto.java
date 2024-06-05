a74045f48b76e684f34a61d00f37072dab90197d
suyong <edgarlee9364@gmail.com>
2024-06-06 00:59:26 +0900

    feat: 댓글 CRUD 구현

---
+++ src/main/java/com/example/HiBuddy/domain/comment/dto/response/CommentsResponseDto.java
@@ -0,0 +1,48 @@
+package com.example.HiBuddy.domain.comment.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+public class CommentsResponseDto {
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class UserDto {
+        private Long userId;
+        private String nickname;
+        private String profileUrl;
+    }
+
+    @Builder
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class CommentDto {
+        private Long commentId;
+        private String comment;
+        private boolean isAuthor;
+        private String createdAt;
+        private UserDto user;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class CommentsInfoPageDto {
+        private List<CommentDto> result;
+        private int totalPages;
+        private int totalElements;
+        private boolean isFirst;
+        private boolean isLast;
+        private int size;
+        private int number;
+        private int numberOfElements;
+    }
+}
